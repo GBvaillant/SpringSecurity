28f6591c3b8bbb48ae5c6ffb32bd3bef1de881f1
GBvaillant <vaillantgabriel@gmail.com>
2024-08-06 14:26:23 -0300

    feat: add configs security

---
+++ src/main/java/com/example/apiTeste/infra/security/CustomUserDetailService.java
@@ -0,0 +1,23 @@
+package com.example.apiTeste.infra.security;
+
+import com.example.apiTeste.models.UserModel;
+import com.example.apiTeste.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
+
--- src/main/java/com/example/apiTeste/repositories/UserRepository.java
+++ src/main/java/com/example/apiTeste/repositories/UserRepository.java
@@ -4,8 +4,10 @@ import com.example.apiTeste.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    Optional<UserModel> findByEmail(String email);
 }
